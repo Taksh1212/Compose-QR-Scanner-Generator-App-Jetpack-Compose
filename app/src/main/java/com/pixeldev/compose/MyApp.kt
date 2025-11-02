d431dfdf583414d3b1bcc4da59da59e226e059c0
Dinesh2510 <dineshchavan7708@outlook.com>
2025-11-02 13:10:33 +0530

    CopyRightProfile Added

--- app/src/main/java/com/pixeldev/compose/MyApp.kt
+++ app/src/main/java/com/pixeldev/compose/MyApp.kt
@@ -1,3 +1,16 @@
+/*
+ * Copyright (C) 2025 Dinesh2510
+ * All rights reserved.
+ *
+ * Author: Dinesh2510
+ * Date: 2025-11-02
+ *
+ * This file is part of the QR Scan project.
+ *
+ * GitHub: https://github.com/Dinesh2510
+ * YouTube Channel: https://www.youtube.com/@pixeldesigndeveloper
+ *
+ */
 package com.pixeldev.compose
 
 import android.app.Application
